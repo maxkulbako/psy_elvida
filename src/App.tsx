b0b45083a2e3a86d53236b8dcec90f5052f80948
maxkulbako <mkulbako@gmail.com>
2024-05-16 10:52:11 +0200

    add about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.scss";
+import { About } from "./components/About_section/About";
 import { Header } from "./components/Header/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <About />
     </>
   );
 }
---
+++ src/components/About_section/About.tsx
@@ -0,0 +1,41 @@
+import styles from "./About.module.scss";
+import About_img from "../../assets/about_img.jpeg";
+import { CallBackButton } from "../Button/СallBackBtn";
+
+export const About = () => {
+  return (
+    <div className={styles.about_container}>
+      <div className={styles.title}>
+        <p>НАПРАВЛЮ ТЕБЕ</p>
+        <p>
+          ЯК <span>ВИРІШИТИ</span>
+        </p>
+        <p>
+          КЛАДНІ <span>СИТУАЦІЇ</span>
+        </p>
+      </div>
+      <div className={styles.text_wrapper}>
+        <p>Психологиня-консультант.</p>
+        <p>Допомагаю людям віднайти гармонію.</p>
+        <p>Вселяю віру та любов до себе.</p>
+        <p>Пізнай себе по-новому!</p>
+      </div>
+      <div className={styles.btn_wrapper}>
+        <CallBackButton />
+      </div>
+      <div className={styles.img_wrapper}>
+        <div className={styles.quote}>
+          <p>
+            "Усі відповіді уже в <span>тобі.</span>
+          </p>
+          <p>
+            Я тільки <span>допоможу</span> їх віднайти"
+          </p>
+        </div>
+        <div className={styles.img_container}>
+          <img src={About_img} alt="About_img" />
+        </div>
+      </div>
+    </div>
+  );
+};
