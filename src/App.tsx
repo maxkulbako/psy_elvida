347345de5d87ae2598e25e8a23aeb69255978e84
maxkulbako <mkulbako@gmail.com>
2024-05-16 15:38:15 +0200

    add themes section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.scss";
 import { About } from "./components/About_section/About";
 import { Header } from "./components/Header/Header";
 import { Story } from "./components/Story_section/Story";
+import { Themes } from "./components/Themes_section/Themes";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <About />
       <Story />
+      <Themes />
     </>
   );
 }
---
+++ src/components/Themes_section/Themes.tsx
@@ -0,0 +1,97 @@
+import styles from "./Themes.module.scss";
+import Themes_img from "../../assets/themes_img.jpeg";
+import Questions_img from "../../assets/questions_img.jpeg";
+
+export const Themes = () => {
+  return (
+    <div className={styles.themes_container}>
+      <p className={styles.page_number}>02</p>
+      <div className={styles.themes_block}>
+        <div className={styles.text_block_wrapper}>
+          <p className={styles.title}>Теми, на які Ти знайдеш відповіді</p>
+          <div className={styles.themes_wrapper}>
+            <div className={styles.thema_wrapper}>
+              <p>ВІДНОСИНИ В СІМʼЇ, РОДИНІ</p>
+            </div>
+            <div className={styles.thema_wrapper}>
+              <p>ВИГОРАННЯ</p>
+            </div>
+            <div className={styles.thema_wrapper}>
+              <p>СТОСУНКИ З ПАРТНЕРОМ</p>
+            </div>
+            <div className={styles.thema_wrapper}>
+              <p>САМООЦІНКА</p>
+            </div>
+            <div className={styles.thema_wrapper}>
+              <p>ПОШУК СЕБЕ</p>
+            </div>
+            <div className={styles.thema_wrapper}>
+              <p>САМОРЕАЛІЗАЦІЯ</p>
+            </div>
+            <div className={styles.thema_wrapper}>
+              <p>БАЛАНС В ЖИТТІ</p>
+            </div>
+            <div className={styles.thema_wrapper}>
+              <p>ХТО Я?</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.img_container}>
+          <img src={Themes_img} alt="Themes_img" />
+        </div>
+      </div>
+      <p className={styles.page_number} style={{ alignSelf: "flex-start" }}>
+        03
+      </p>
+      <div
+        className={styles.themes_block}
+        style={{ flexDirection: "row-reverse" }}
+      >
+        <div className={styles.text_block_wrapper}>
+          <p
+            className={styles.title}
+            style={{ alignSelf: "end", width: "412px" }}
+          >
+            Запити, з якими звертаються клієнти
+          </p>
+          <div className={styles.themes_wrapper}>
+            <div className={styles.question_wrapper}>
+              <p>Я заплуталась з правильним рішенням</p>
+            </div>
+            <div className={styles.question_wrapper}>
+              <p>Мене не розуміють і не чують рідні</p>
+            </div>
+            <div className={styles.question_wrapper}>
+              <p>Не розумію куди рухатись далі </p>
+            </div>
+            <div className={styles.question_wrapper}>
+              <p>Не знаю, що зі мною, але я якась не така</p>
+            </div>
+            <div className={styles.question_wrapper}>
+              <p>Втомилась від постійної гонки</p>
+            </div>
+            <div className={styles.question_wrapper}>
+              <p>Відчуття самозванця</p>
+            </div>
+            <div className={styles.question_wrapper}>
+              <p>Відчуваю в собі сили зробити ривок, але боюсь</p>
+            </div>
+            <div className={styles.question_wrapper}>
+              <p>Не можу казати «ні»</p>
+            </div>
+            <div className={styles.question_wrapper}>
+              <p>Загубила себе. Не знаю з чого почати»</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.img_container}>
+          <img
+            src={Questions_img}
+            alt="Themes_img"
+            style={{ transform: "scaleX(-1)" }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
